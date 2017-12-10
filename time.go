218a5adfb5fbb01df1dfb193cf969c1432b7b296
nakanoh <example@gmail.com>
2017-12-11 02:49:00 +0900

    update

--- time.go
+++ time.go
@@ -9,9 +9,9 @@ import (
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         t := time.Now()
-	      format := "2006/01/02 15:04:05"
-	      year := t.Format(format)
-	      w.Write([]byte(year))
+        format := "2006/01/02 15:04:05"
+        year := t.Format(format)
+        w.Write([]byte(year))
     })
 
     if err := http.ListenAndServe(":3000", nil); err != nil {
